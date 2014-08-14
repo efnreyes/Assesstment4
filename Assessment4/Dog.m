d2f56f7ce734b7ed939b1a3dccc84c3afa7d1807
Efrén Reyes <efren.reyes@globant.com>
2014-08-14 14:10:56 -0500

    Added Person and Dog classes

---
+++ Assessment4/Dog.m
@@ -0,0 +1,19 @@
+//
+//  Dog.m
+//  Assessment4
+//
+//  Created by Efrén Reyes Torres on 8/14/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import "Dog.h"
+
+
+@implementation Dog
+
+@dynamic name;
+@dynamic breed;
+@dynamic color;
+@dynamic owner;
+
+@end
